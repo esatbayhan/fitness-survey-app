374689499e47a0780e70013ec1f0c4c6a5f5f295
Esat Sefa Bayhan <esatbf@gmail.com>
2022-05-14 13:09:36 +0200

    Added accelerometer test activity

--- app/src/main/java/de/rub/selab22a15/MainActivity.java
+++ app/src/main/java/de/rub/selab22a15/MainActivity.java
@@ -2,13 +2,29 @@ package de.rub.selab22a15;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnOpenActivityAccelerometerTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnOpenActivityAccelerometerTest = findViewById(R.id.btnOpenActivityAccelerometerTest);
+        btnOpenActivityAccelerometerTest.setOnClickListener(new btnOpenActivityAccelerometerTestOnClickListener());
+    }
+
+    private class btnOpenActivityAccelerometerTestOnClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, AccelerometerTestActivity.class);
+            startActivity(intent);
+        }
     }
 }
