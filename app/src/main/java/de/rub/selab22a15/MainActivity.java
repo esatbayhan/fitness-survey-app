61a1c0a62df533dd569f15c1ecb0b19ca9fea6ee
Esat Sefa Bayhan <esat.bayhan@rub.de>
2022-06-23 13:41:26 +0200

    Initial commit for changed design architecture

--- app/src/main/java/de/rub/selab22a15/MainActivity.java
+++ app/src/main/java/de/rub/selab22a15/MainActivity.java
@@ -1,65 +1,45 @@
 package de.rub.selab22a15;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+
+import de.rub.selab22a15.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button btnOpenActivityAccelerometerTest;
-    private Button btnOpenActivitySurveyTest;
-    private Button btnOpenActivityGPS;
-    private Button btnOpenActivityExport;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        switchFragment(new HomeFragment());
 
-        btnOpenActivityAccelerometerTest = findViewById(R.id.btnOpenActivityAccelerometerTest);
-        btnOpenActivitySurveyTest = findViewById(R.id.btnOpenActivitySurveyTest);
-        btnOpenActivityGPS = findViewById(R.id.btnOpenActivityGPS);
-        btnOpenActivityExport = findViewById(R.id.btnOpenActivityExport);
+        binding.bottomNavigation.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.btmNavHome:
+                    switchFragment(new HomeFragment());
+                    break;
+                case R.id.btmNavActivity:
+                    switchFragment(new ActivityFragment());
+                    break;
+                case R.id.btmNavExport:
+                    switchFragment(new ExportFragment());
+                    break;
+                case R.id.btmNavSettings:
+                    switchFragment(new SettingsFragment());
+                    break;
+            }
 
-        btnOpenActivityAccelerometerTest.setOnClickListener(new btnOpenActivityAccelerometerTestOnClick());
-        btnOpenActivitySurveyTest.setOnClickListener(new btnOpenActivitySurveyTestOnClick());
-        btnOpenActivityGPS.setOnClickListener(new btnOpenActivityGPS());
-        btnOpenActivityExport.setOnClickListener(new btnOpenActivityExport());
+            return true;
+        });
     }
 
-    private class btnOpenActivityAccelerometerTestOnClick implements View.OnClickListener {
-        @Override
-        public void onClick(View v) {
-            Intent intent = new Intent(MainActivity.this, AccelerometerTestActivity.class);
-            startActivity(intent);
-        }
-    }
-
-    private class btnOpenActivityGPS implements View.OnClickListener {
-        @Override
-        public void onClick(View v) {
-            Intent intent = new Intent (MainActivity.this, GPSActivity.class);
-            startActivity(intent);
-        }
-    }
-
-    private class btnOpenActivityExport implements View.OnClickListener {
-
-        @Override
-        public void onClick(View v) {
-            Intent intent = new Intent(MainActivity.this, ExportDatabaseActivity.class);
-            startActivity(intent);
-        }
-    }
-
-    private class btnOpenActivitySurveyTestOnClick implements View.OnClickListener {
-        @Override
-        public void onClick(View v) {
-            Intent intent = new Intent(MainActivity.this, SurveyTestActivity.class);
-            startActivity(intent);
-        }
+    private void switchFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frmLytMainActivity, fragment);
+        fragmentTransaction.commit();
     }
 }
