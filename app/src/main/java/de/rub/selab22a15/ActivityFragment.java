30cb4be7ed04191b12f56d794e710d25f4f062dc
Esat Sefa Bayhan <esat.bayhan@rub.de>
2022-06-25 14:32:01 +0200

    Added clock

--- app/src/main/java/de/rub/selab22a15/ActivityFragment.java
+++ app/src/main/java/de/rub/selab22a15/ActivityFragment.java
@@ -12,16 +12,28 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 
+import android.os.SystemClock;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Chronometer;
 import android.widget.Switch;
 import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
 
 import de.rub.selab22a15.db.Accelerometer;
 import de.rub.selab22a15.db.AccelerometerRepository;
 
 public class ActivityFragment extends Fragment {
+    private MaterialButton btnStartActivity;
+    private MaterialButton btnPauseActivity;
+    private MaterialButton btnResumeActivity;
+    private MaterialButton btnStopActivity;
+    private Chronometer cmtActivity;
+    private boolean isTracking;
+    private long pauseOffset;
+
 //    private float accelerometerX, accelerometerY, accelerometerZ;
 //    private final float EPSILON = 0.1f;
 //
@@ -40,7 +52,64 @@ public class ActivityFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_activity, container, false);
     }
 
-//    @Override
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        FragmentActivity activity = getActivity();
+        btnStartActivity = activity.findViewById(R.id.btnStartActivity);
+        btnPauseActivity = activity.findViewById(R.id.btnPauseActivity);
+        btnResumeActivity = activity.findViewById(R.id.btnResumeActivity);
+        btnStopActivity = activity.findViewById(R.id.btnStopActivity);
+        cmtActivity = activity.findViewById(R.id.cmtActivity);
+
+        btnStartActivity.setOnClickListener(view1 -> {
+            cmtActivity.setBase(SystemClock.elapsedRealtime());
+            cmtActivity.start();
+
+            disableButton(btnStartActivity);
+            enableButton(btnPauseActivity);
+            btnStopActivity.setClickable(true);
+        });
+
+        btnPauseActivity.setOnClickListener(view1 -> {
+            cmtActivity.stop();
+            pauseOffset = SystemClock.elapsedRealtime() - cmtActivity.getBase();
+
+            disableButton(btnPauseActivity);
+            enableButton(btnResumeActivity);
+        });
+
+        btnResumeActivity.setOnClickListener(view1 -> {
+            cmtActivity.setBase(SystemClock.elapsedRealtime() - pauseOffset);
+            cmtActivity.start();
+
+            disableButton(btnResumeActivity);
+            enableButton(btnPauseActivity);
+        });
+
+        btnStopActivity.setOnClickListener(view1 -> {
+            cmtActivity.stop();
+            cmtActivity.setBase(SystemClock.elapsedRealtime());
+
+            disableButton(btnPauseActivity);
+            disableButton(btnResumeActivity);
+            enableButton(btnStartActivity);
+            btnStopActivity.setClickable(false);
+        });
+    }
+
+    private void disableButton(MaterialButton button) {
+        button.setEnabled(false);
+        button.setVisibility(View.GONE);
+    }
+
+    private void enableButton(MaterialButton button) {
+        button.setEnabled(true);
+        button.setVisibility(View.VISIBLE);
+    }
+
+    //    @Override
 //    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 //        super.onViewCreated(view, savedInstanceState);
 //
