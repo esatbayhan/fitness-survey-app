ddf628eabf05788f4b6331bd8c341bef1d1dbb99
Esat Sefa Bayhan <esat.bayhan@rub.de>
2022-06-29 17:38:07 +0200

    Initially added fragment for survey mood

---
+++ app/src/main/java/de/rub/selab22a15/fragments/SurveyMoodFragment.java
@@ -0,0 +1,20 @@
+package de.rub.selab22a15.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import de.rub.selab22a15.R;
+
+public class SurveyMoodFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_survey_mood, container, false);
+    }
+}
