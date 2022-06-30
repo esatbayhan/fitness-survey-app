6e20dd277082f84a93d2eec0a8f8ec9fed916e26
Esat Sefa Bayhan <esat.bayhan@rub.de>
2022-06-30 22:43:53 +0200

    First version of home screen

--- app/src/main/java/de/rub/selab22a15/HomeFragment.java
+++ app/src/main/java/de/rub/selab22a15/HomeFragment.java
@@ -6,15 +6,21 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.card.MaterialCardView;
+
 import de.rub.selab22a15.activities.SurveyActivity;
 
 public class HomeFragment extends Fragment {
+    MaterialCardView cardViewSurvey;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -26,10 +32,18 @@ public class HomeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        Button button = requireActivity().findViewById(R.id.buttonHomeFragmentSurvey);
-        button.setOnClickListener(v -> {
-            Intent intent = new Intent(requireActivity(), SurveyActivity.class);
-            startActivity(intent);
-        });
+        FragmentActivity activity = requireActivity();
+
+        cardViewSurvey = activity.findViewById(R.id.cardViewSurvey);
+        MaterialButton buttonCardViewSurveyRemind = activity.findViewById(R.id.buttonCardViewSurveyRemind);
+        MaterialButton buttonCardViewSurveyStart = activity.findViewById(R.id.buttonCardViewSurveyStart);
+
+        buttonCardViewSurveyRemind.setOnClickListener(v -> cardViewSurvey.setVisibility(View.GONE));
+        buttonCardViewSurveyStart.setOnClickListener(v -> startSurvey());
+    }
+
+    private void startSurvey() {
+        Intent surveyIntent = new Intent(requireActivity(), SurveyActivity.class);
+        startActivity(surveyIntent);
     }
 }
