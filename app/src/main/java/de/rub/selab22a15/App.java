fb4c28a77b9207d0527da8510fc8db1c12432b58
Esat Sefa Bayhan <esat.bayhan@rub.de>
2022-07-02 17:20:41 +0200

    Start work manager if user stops app

--- app/src/main/java/de/rub/selab22a15/App.java
+++ app/src/main/java/de/rub/selab22a15/App.java
@@ -18,21 +18,17 @@ import de.rub.selab22a15.workers.AccelerometerRecordWorker;
 
 
 public class App extends Application {
-    public static final int CHANNEL_ID_NR_ACTIVITY_RECORD = 1;
     public static final String CHANNEL_ID_ACTIVITY_RECORD = "SERVICE_CHANNEL_ACTIVITY_RECORD";
-    public static final String CHANNEL_NAME_ACTIVITY_RECORD = "SERVICE_CHANNEL_ACTIVITY_RECORD";
+    public static final String CHANNEL_NAME_ACTIVITY_RECORD = CHANNEL_ID_ACTIVITY_RECORD;
 
     public static final String TAG_PASSIVE_RECORDING = "PASSIVE_RECORDING";
-
-    public static final String CHANNEL_ID_TEST = "CHANNEL_TEST";
-    public static final String CHANNEL_NAME_TEST = "CHANNEL_TEST";
 
     private static Application INSTANCE;
     private static boolean IS_RUNNING_FOREGROUND;
 
-    private static long LOW_DURATION_SECONDS = 75;
-    private static long MEDIUM_DURATION_SECONDS = 125;
-    private static long HIGH_DURATION_SECONDS = 175;
+    private static final long LOW_DURATION_SECONDS = 75;
+    private static final long MEDIUM_DURATION_SECONDS = 125;
+    private static final long HIGH_DURATION_SECONDS = 175;
 
     public static Application getInstance() {
         return INSTANCE;
@@ -49,18 +45,17 @@ public class App extends Application {
 
         ProcessLifecycleOwner.get().getLifecycle().addObserver(new AppLifecycleListener());
         createNotificationChannelService();
-        createWorkManager();
     }
 
     private void createNotificationChannelService() {
-        NotificationChannel serviceChannel = new NotificationChannel(
+        NotificationChannel serviceNotificationChannel = new NotificationChannel(
                 CHANNEL_ID_ACTIVITY_RECORD,
                 CHANNEL_NAME_ACTIVITY_RECORD,
                 NotificationManager.IMPORTANCE_LOW
         );
 
         NotificationManager manager = getSystemService(NotificationManager.class);
-        manager.createNotificationChannel(serviceChannel);
+        manager.createNotificationChannel(serviceNotificationChannel);
     }
 
     private void createWorkManager() {
@@ -74,7 +69,7 @@ public class App extends Application {
         WorkManager.getInstance(getApplicationContext())
                 .enqueueUniquePeriodicWork(
                         TAG_PASSIVE_RECORDING,
-                        ExistingPeriodicWorkPolicy.KEEP,
+                        ExistingPeriodicWorkPolicy.REPLACE,
                         workRequest
                 );
     }
@@ -91,6 +86,7 @@ public class App extends Application {
         public void onStop(@NonNull LifecycleOwner owner) {
             DefaultLifecycleObserver.super.onStop(owner);
             IS_RUNNING_FOREGROUND = false;
+            createWorkManager();
         }
     }
 }
